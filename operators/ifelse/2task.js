53894319fb37e3e57e558d23ac3c4932aec5171d
Aleksandr Danilin <Alex39292@mail.ru>
2020-10-06 12:53:30 +0300

    "Make tasks"

---
+++ operators/ifelse/2task.js
@@ -0,0 +1,5 @@
+let login = 'Директор'; 
+let message = (login == 'Сотрудник') ? 'Привет' :
+  (login == 'Директор') ? 'Здравствуйте' :
+  (login == '') ? 'Нет логина' : '';
+  console.log(message);
