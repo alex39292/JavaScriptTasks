ebf07aca132e405c704f75f1a1c7aa958786e05a
Aleksandr Danilin <Alex39292@mail.ru>
2020-10-08 13:04:32 +0300

    Fix veriable styles to const

--- Arrays/task.js
+++ Arrays/task.js
@@ -1,6 +1,6 @@
-let styles = ["Джаз", "Блюз"];
+const styles = ["Джаз", "Блюз"];
 styles.push("Рок-н-ролл");
 styles[Math.floor((styles.length - 1) / 2)] = "Классика";
-console.log(styles.shift());
+console.log(styles.shift()); //Джаз
 styles.unshift("Рэп", "Регги");
-console.log(String(styles));
+console.log(String(styles)); //Рэп,Регги,Классика,Рок-н-ролл
