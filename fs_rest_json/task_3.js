65e69159359f273f5e252537648c4156a4ba80db
Aleksandr Danilin <Alex39292@mail.ru>
2020-10-19 14:10:55 +0300

    Make 3d task with using only one file

--- fs_rest_json/task_3.js
+++ fs_rest_json/task_3.js
@@ -2,6 +2,7 @@
 
 const path = require("path")
 const fs = require("fs")
+const xlsx = require("node-xlsx")
 
 let inputPath
 let outputPath
@@ -17,19 +18,58 @@ if (process.argv[4] === "--outputDir") {
     outputPath = path.join(__dirname, "/resources/task_3/")
 }
 
-const json = require("./resources/task_3/jsonToXlsx.json")
+let array = [...[]]
+const json = require(inputPath)
 
-function r(json) {
-    for (let i in json) {
-        if (typeof json[i] !== "object" || Array.isArray(json[i])) {
-            console.log(i + " - " + json[i])
-        } else {
-            if (Array.isArray(json[i])) {
-                r(json[i])
-            }
-            r(json[i])
+traverse(json)
+
+function traverse(o) {
+    let i;
+    for (let k in o) {
+        i = o[k];
+        if (typeof i === "string" || typeof i === "number" || typeof i === "boolean") {
+            console.log(`${k} : ${i}`);
+            array.push([], [k, i])
+        } else if (typeof i === "object") {
+            console.log(`${k} -->\n`)
+            array.push([],[k])
+            traverse(i);
         }
     }
 }
 
-r(json)
+const buffer = xlsx.build([{ name: "mySheetName", data: array }])
+
+fs.writeFile(inputPath.replace(/\.[^\.]+$/, '.xlsx'), buffer, (err) => {
+    if (err) {
+        throw err
+    }
+})
+
+function fromDir(startPath, filter, callback) {
+    let filesArr = []
+
+    if (!fs.existsSync(startPath)) {
+        console.log("no dir ", startPath);
+        return;
+    }
+
+    var files = fs.readdirSync(startPath)
+    for (var i = 0; i < files.length; i++) {
+        var filename = path.join(startPath, files[i])
+        var stat = fs.lstatSync(filename)
+        if (!stat.isDirectory()) {
+            if (filter.test(filename)) {
+                callback(filename)
+            }
+        }
+    }
+}
+
+function findJsonFiles(path) {
+    let arr = []
+    fromDir(path, /\.json$/, function (filename) {
+        arr.push(filename)
+    })
+    return arr
+}
---
+++ fs_rest_json/task_5/app.js
@@ -0,0 +1,5 @@
+"use strict"
+
+const todo = require("./module/todo.js")
+
+todo.start()
