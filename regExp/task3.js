7103023dfca9da57aac7216b2f26cb012767256b
Aleksandr Danilin <Alex39292@mail.ru>
2020-10-21 16:39:54 +0300

    Make 3 task

---
+++ regExp/task3.js
@@ -0,0 +1,15 @@
+function parse(str) {
+    const regExp = /(-?\d+(?:\.\d+)?)\s*([\*\/\-\+])\s*(-?\d+(?:\.\d+)?)/
+
+    const result = str.match(regExp)
+
+    if (!result) {
+        result = []
+    } else {
+        result.shift()
+    }
+
+    return result
+}
+
+console.log(parse("1.2 * 3.4"))
